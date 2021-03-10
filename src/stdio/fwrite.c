ef324c8078ba7b9fadd14adfefaa73290aadeadc
Lance Hart <shipsimfan@hotmail.com>
2021-03-10 15:31:41 -0500

    Added file writing to fwrite

--- src/stdio/fwrite.c
+++ src/stdio/fwrite.c
@@ -8,6 +8,9 @@ size_t fwrite(const void* ptr, size_t size, size_t count, FILE* stream) {
     case FILE_TYPE_DEVICE:
         return SystemCall(SYSTEM_CALL_DEVICE_WRITE_STREAM, stream->descriptor, 0, (uint64_t)ptr, size * count);
 
+    case FILE_TYPE_FILE:
+        return SystemCall(SYSTEM_CALL_FILE_WRITE, stream->descriptor, (uint64_t)ptr, size * count, 0);
+
     default:
         return -1;
     }
