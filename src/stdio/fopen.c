952058a3dbfb3fb7d63948709b3d3b31608ed36e
Lance Hart <shipsimfan@hotmail.com>
2021-03-18 22:31:53 -0400

    Added flags to file opening

--- src/stdio/fopen.c
+++ src/stdio/fopen.c
@@ -1,11 +1,29 @@
 #include <stdio.h>
 
 #include <stdlib.h>
+#include <string.h>
 
 #include "../defs.h"
 
+#define OPEN_READ 1
+#define OPEN_WRITE 2
+
 FILE* fopen(const char* filename, const char* mode) {
-    int descriptor = SystemCall(SYSTEM_CALL_FILE_OPEN, (uint64_t)filename, 0, 0, 0);
+    int flags = 0;
+    size_t modeLen = strlen(mode);
+    if (modeLen > 0) {
+        if (mode[0] == 'r')
+            flags |= OPEN_READ;
+        else if (mode[0] == 'w' || mode[0] == 'a')
+            flags |= OPEN_WRITE;
+
+        if (modeLen > 1) {
+            if (mode[1] == '+')
+                flags |= OPEN_READ | OPEN_WRITE;
+        }
+    }
+
+    int descriptor = SystemCall(SYSTEM_CALL_FILE_OPEN, (uint64_t)filename, flags, 0, 0);
     if (descriptor < 0)
         return NULL;
 
